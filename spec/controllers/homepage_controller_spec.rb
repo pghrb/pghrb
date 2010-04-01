dc8e945700784be337926d8f78255ee7cba99fb6
Max Hawkins <maxhawkins@gmail.com>
2010-03-31 21:29:20 -0400

    Added HomepageController

---
+++ spec/controllers/homepage_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe HomepageController do
+
+  #Delete this example and add some real ones
+  it "should use HomepageController" do
+    controller.should be_an_instance_of(HomepageController)
+  end
+
+end
