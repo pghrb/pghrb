90766c0890e238b98afd156a76e0a9a09bd501e4
Max Hawkins <maxhawkins@gmail.com>
2010-04-01 14:38:09 -0400

    Linking up meetings.

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "homepage"
+  
+  map.resources :meetings
 
   # See how all your routes lay out with "rake routes"
 
